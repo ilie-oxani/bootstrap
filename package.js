e6bc1045d564745a09f927c4f3f568f65ee2dca6
Oxani Ilie <oxani.ilie@gmail.com>
2017-09-08 10:28:20 +0300

    rename pacage

--- package.js
+++ package.js
@@ -3,10 +3,10 @@
 /* global Package:true */
 
 Package.describe({
-  name: 'twbs:bootstrap',  // https://atmospherejs.com/twbs/bootstrap
+  name: 'ilie-oxani:bootstrap-beta',  // https://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
   version: '4.0.0-beta',
-  git: 'https://github.com/twbs/bootstrap.git'
+  git: 'https://github.com/ilie-oxani/bootstrap-beta.git'
 });
 
 Package.onUse(function (api) {
